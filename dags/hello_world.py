1c9978a908f3a8956a94cf14e8e2fd789ff2bc7a
Justin Wagg <jmwagg@gmail.com>
2020-05-26 23:45:32 +0100

    Update hello_world DAG

--- dags/hello_world.py
+++ dags/hello_world.py
@@ -17,10 +17,16 @@ dag = DAG(
     catchup=False,
 )
 
-t1 = BashOperator(
+say_hey = BashOperator(
     task_id='say_hey',
     bash_command='echo "Hello World"',
     dag=dag,
 )
 
+happy_day = BashOperator(
+    task_id='happy_day',
+    bash_command='echo "Happy $(date +"%A")!"',
+    dag=dag,
+)
 
+say_hey >> happy_day
